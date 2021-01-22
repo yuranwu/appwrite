d97466f4b7bc9ffc9b940c1f92d149a960a8919f
Eldad Fux <eldad.fux@gmail.com>
2021-01-22 11:23:32 +0200

    Deploy dart images

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,1 +1,3 @@
-echo 'Nothing to deploy right now.'
+# echo 'Nothing to deploy right now.'
+
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/ppc64le -t appwrite/env-dart-2.10.4:1.0.0 ./docker/environments/dart-2.10.4/ --push
