a735b62191e2da2d171563fa36b77cd99c51016b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-10-18 09:39:38 +0200

    fix whitespace

--- app/tasks/sdks.php
+++ app/tasks/sdks.php
@@ -204,7 +204,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
                 $gitUrl = $language['gitUrl'];
                 $gitBranch = $language['gitBranch'];
 
-                
                 if(!$production) {
                     $gitUrl = 'git@github.com:aw-tests/'.$language['gitRepoName'].'.git';
                 }
